9968e5c97b77429e0f9ec21e91a15c88a18dc010
James Miller <bensie@gmail.com>
2011-08-10 14:56:56 -0700

    Revert "Try forcing SSL"
    This reverts commit 9ed56a8ed2dc9d5dec86561e1d84c61a4d26e977.

--- lib/httpi/adapter/net_http.rb
+++ lib/httpi/adapter/net_http.rb
@@ -77,8 +77,7 @@ module HTTPI
       end
 
       def setup_client(request)
-        client.use_ssl = true
-        client.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        client.use_ssl = request.ssl?
         client.open_timeout = request.open_timeout if request.open_timeout
         client.read_timeout = request.read_timeout if request.read_timeout
       end
